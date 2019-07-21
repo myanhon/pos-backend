29bbc7fac81926fa0deec5084ba8fe17b073bff5
Blank <blankngnl@protonmail.com>
2019-07-21 20:12:06 +0200

    get orders done

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,10 +1,35 @@
 const order = require('../models/orderModel');
 const bodyParser = require('body-parser');
 const product = require('../models/productModel');
+const selectString = 'product quantity';
+
 
 module.exports = function (app) {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+
+    app.get('/api/orders',(req,res)=>{
+        order.find().select(selectString).exec().then(docs =>{
+            res.status(200).json({
+                count:docs.length,
+                orders: docs.map(doc =>{
+                    return {
+                        _id: doc._id,
+                        product: doc.product,
+                        quantity: doc.quantity,
+                        request:{
+                            type: 'GET',
+                            url: 'http://localhost:3000/api/orders' + doc
+                        }
+                    };
+                })
+            });
+        }).catch(err =>{
+            res.status(500).json({
+                error: err
+            })
+        })
+    });
 
     app.post('/api/order', (req, res) => {
         const newOrder = order({
@@ -12,7 +37,6 @@ module.exports = function (app) {
             product: req.body.productId
         });
         newOrder.save().then(result=>{
-
             res.status(200).json(result);
         }).catch(err =>{
             res.status(500).json({
@@ -21,8 +45,5 @@ module.exports = function (app) {
         })
 
     });
-
-
-
 
 };
--- models/orderModel.js
+++ models/orderModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-    products: {type:mongoose.Schema.Types.ObjectId, ref:'Product'},
+    product: {type: mongoose.Schema.Types.ObjectId, ref:'Product'},
     quantity: {type: Number, default:1}
 });
 
