8cbc65b2b92f7fcdfb09d808a1f0c7b7fa62ac31
myanhon <pluh@protonmail.com>
2020-02-02 16:36:45 +0100

    changed to proper response

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,48 +1,44 @@
 const Cart = require('../models/cartModel');
-const querystring = require('querystring');
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
 module.exports = function (app) {
-    //TODO or not TODO
-    app.get('/api/checkout', (req, res,) => {
-        if (!req.session.cart) {
-            //redirect?
-            console.log('get welloe');
-            res.send('error');
+    app.get('/api/checkout', (req, res) => {
+        if (!req.session.cart ) {
+            res.json({
+                isActive: false
+            });
+        }else{
+            res.json({
+                isActive: true,
+            })
         }
-        console.log('WE FKING MADE IT',req.session.cart);
-
-        const cart = new Cart(req.session.cart);
-        res.json({
-            totalPrice: cart.totalPrice
-        })
     });
 
     app.post('/api/checkout', (req, res) => {
         if (!req.session.cart) {
-            //should redirect
-            console.log('geen cart huh?');
-            return
-        }
-        const cart = new Cart(req.session.cart);
-        // Token is created using Stripe Checkout or Elements!
-       // Get the payment token ID submitted by the form:
-        stripe.charges.create({
-            amount: cart.totalPrice*100,
-            currency: "USD",
-            description: 'Example charge',
-            source:req.body,
-        }).then(() => {
-            console.log('Successfully bought product!');
-            req.session.cart = null;
-            res.status(200).json({
-                message: 'Successfully bought product!'
+            res.json({
+                message: "Cart is Empty"
             });
-        }).catch(error => {
-            // console.log("error g:", error);
-            return res.status(500).json({
-                error: error
-            })
-        });
-
+        } else {
+            const cart = new Cart(req.session.cart);
+            // Token is created using Stripe Checkout or Elements!
+            // Get the payment token ID submitted by the form:
+            stripe.charges.create({
+                amount: cart.totalPrice * 100,
+                currency: "USD",
+                description: 'Example charge',
+                source: req.body,
+            }).then(() => {
+                console.log('Successfully bought product!');
+                req.session.cart = null;
+                res.status(200).json({
+                    message: 'Successfully bought product!'
+                });
+            }).catch(error => {
+                console.log("error g:", error);
+                return res.status(500).json({
+                    error: error
+                })
+            });
+        }
     });
 };
